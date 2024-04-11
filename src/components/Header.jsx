61380c5f14fd7de9fff77430c8c4a5ab493cc9f3
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-11 17:27:47 +0100

    total cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "../UI/Button";
+import CartContext from "../store/CartContext";
 export default function Header() {
+  const crtCtx = useContext(CartContext);
+  const totalCartItems = crtCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
@@ -8,7 +15,7 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
