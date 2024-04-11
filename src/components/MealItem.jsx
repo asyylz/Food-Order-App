df841c68c312abc85175165d3a1ab90f3f2a2d16
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-11 14:20:04 +0100

    button added to mealitem

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,17 +1,20 @@
 import { currencyFormatter } from "../utils/formatting";
+import Button from "../UI/Button";
 export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-      {/* meal.image is relative path so we added backend hard-coded part */}
-        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} /> 
+        {/* meal.image is relative path so we added backend hard-coded part */}
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-            <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
