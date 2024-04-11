19605acedca64b561d9c72348b48a85cd45a074f
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-11 14:07:55 +0100

    mealitem

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -2,7 +2,8 @@ export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={meal.image} alt={meal.name} />
+      {/* meal.image is relative path so we added backend hard-coded part */}
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} /> 
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">{meal.price}</p>
