f1f34e433d12da3dc5bb6bd15249c70b4b85295d
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-12 13:49:59 +0100

    update hook

--- src/hooks/useHttp.jsx
+++ src/hooks/useHttp.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 async function sendHttpRequest(url, config) {
   // this function dealing with  sending requests
@@ -14,21 +14,24 @@ async function sendHttpRequest(url, config) {
   return resData;
 }
 
-export default function useHttp() {
+export default function useHttp(url, config) {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
   // this function dealing with updating the states
-  async function sendRequest() {
+  const sendRequest = useCallback(async function sendRequest() {
     setIsLoading(true);
     try {
-      const restData = sendHttpRequest();
+      const restData = sendHttpRequest(url, config);
       setData(restData);
     } catch (error) {
       setError(error.message || "Something went wrong");
     }
     setIsLoading(false);
-  }
+  },[url, config]);
+
+  useEffect(() => sendRequest(), [sendRequest]);
+
   return { data, isLoading, error };
 }
