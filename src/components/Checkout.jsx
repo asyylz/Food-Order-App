49507c16384db92edbf52b8b279c7e95f605cebb
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-12 12:32:02 +0100

    update

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,17 +3,23 @@ import Modal from "../UI/Modal";
 import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../utils/formatting";
 import Input from "../UI/Input";
+import Button from "../UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
 
   return (
-    <Modal>
+    <Modal open={userProgressCtx.progress === "checkout"}>
       <form action="">
         <h2>Checkout</h2>
         <p>Total Amount:{currencyFormatter.format(cartTotal)}</p>
@@ -24,6 +30,11 @@ export default function Checkout() {
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
+        <p className="modal-actions">
+          <Button type="button" onClick={handleClose}>
+            Close
+          </Button>
+        </p>
       </form>
     </Modal>
   );
