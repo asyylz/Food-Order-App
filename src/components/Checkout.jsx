47f85626c06bc9d4624916286087a46873e997a6
asyylz <49663592+asyylz@users.noreply.github.com>
2024-04-12 12:21:43 +0100

    checkout component

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import Modal from "../UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../utils/formatting";
+
+export default function Checkout() {
+  const cartCtx = useContext(CartContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <Modal>
+      <form action="">
+        <h2>Checkout</h2>
+        <p>Total Amount:{currencyFormatter.format(cartTotal)}</p>
+      </form>
+    </Modal>
+  );
+}
